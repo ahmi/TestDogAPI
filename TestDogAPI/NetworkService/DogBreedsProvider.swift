36fa430ef7cbde5342448422bb890f2c82e6704a
Ahmad waqas <message2ahmad@gmail.com>
2021-03-09 16:09:05 +1000

    - Fixes indentation as SwiftLint

--- TestDogAPI/NetworkService/DogBreedsProvider.swift
+++ TestDogAPI/NetworkService/DogBreedsProvider.swift
@@ -13,12 +13,12 @@ public enum APIError: Error {
     case parsingError
     var message: String {
         switch self {
-            case .internalError:
-                return "Internal Error"
-            case .serverError:
-                return "Server Error"
-            case .parsingError:
-                return "Corrputed Data"
+        case .internalError:
+            return "Internal Error"
+        case .serverError:
+            return "Server Error"
+        case .parsingError:
+            return "Corrputed Data"
         }
     }
 }
@@ -26,6 +26,4 @@ public enum APIError: Error {
 public protocol DogBreedsProvider {
     func fetchRandomBreedImageObject(completion: @escaping((Result<DogsBreedModel, APIError>) -> Void))
     func fetchAllBreedsNameList(completion: @escaping((Result<DogsBreedListModel, APIError>) -> Void))
-
-
 }
